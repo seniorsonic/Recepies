668480d1c6c7385c683aae01546ea495c4d54eb7
Sonic <senior.sonic@yandex.ru>
2023-11-22 19:45:19 +0300

    Начало начал

---
+++ js/mane.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 5250,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+});
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => alert("Вы успешно зарегистрировались"));
+
+document.addEventListener("submit", (e) => {
+    e.prevent.Default();
+    e.target.reset();
+});
+const slider = document.querySelector(".swiper-wrapper");
+const countRecipes = document.querySelectorAll(".swiper-slide-text");
+const countSliders = document.querySelectorAll(".swiper-slide");
+if (countRecipes.length > 7 * countSliders.length) {
+    const newT = document.createElement("div");
+    newT.classList.add("swiper-slide");
+    slider.appendChild(newT);
+}
+
+const currentSlider = countSliders[countSliders.length - 1]; // Доступ к последнему слайдеру
