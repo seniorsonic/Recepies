eb31b8331d59f940899237a25eaf482e818e8777
Sonic <senior.sonic@yandex.ru>
2023-12-13 19:47:15 +0300

    рабочий вид

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
     # оригинальный контент:
@@ -23,3 +23,12 @@ def index():
     #     }
     # ]
     # return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+@app.route('/recipe.html')
+def recipe():
+    return render_template('recipe.html')
